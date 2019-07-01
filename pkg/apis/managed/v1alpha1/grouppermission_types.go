018255a580bb98be7894da6dbcf5ca8d26ea5333
Sam Nguyen <samnguyen@localhost.localdomain>
2019-07-01 13:30:05 -0400

    added CRD spec and status as go structs

--- pkg/apis/managed/v1alpha1/grouppermission_types.go
+++ pkg/apis/managed/v1alpha1/grouppermission_types.go
@@ -10,18 +10,69 @@ import (
 // GroupPermissionSpec defines the desired state of GroupPermission
 // +k8s:openapi-gen=true
 type GroupPermissionSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Name of the Group granted permissions by the operator
+	GroupName string `json:"groupName"`
+	// List of permissions applied at Cluster scope
+	// +optional
+	ClusterPermissions []ClusterPermission `json:"clusterPermissions,omitempty"`
+	// List of permissions applied at Namespace scope
+	// +optional
+	Permissions []Permission `json:"permissions,omitempty"`
+}
+
+// ClusterPermission defines a ClusterRole that is bound to the Group
+type ClusterPermission struct {
+	// Name of ClusterRole to bind to the Group as a ClusterRoleBinding
+	ClusterRoleName string `json:"clusterRoleName"`
+}
+
+// Permission deines a Role that is bound to the Group
+// Allowed in specific NameSpaces
+type Permission struct {
+	// ClusterRoleName to bind to the Group as a RoleBindings in allowed Namespaces
+	ClusterRoleName string `json:"clusterRoleName"`
+	// NamespacesAllowedRegex representing allowed Namespaces
+	NamespacesAllowedRegex string `json:"namespacesAllowedRegex,omitempty"`
+	// NamespacesDeniedRegex representing denied Namespaces
+	NamespacesDeniedRegex string `json:"namespacesDeniedRegex,omitempty"`
+	// Flag to indicate if "allow" regex is applied first
+	// If 'true' order is Allow then Deny, Else order is Deny then Allow
+	AllowFirst bool `json:"allowFirst"`
 }
 
 // GroupPermissionStatus defines the observed state of GroupPermission
 // +k8s:openapi-gen=true
 type GroupPermissionStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	// List of conditions for the CR
+	Conditions []Condition `json:"conditions,omitempty"`
+	// State that this condition represents
+	State string `json:"state"`
 }
+
+// Condition defines a single condition of running the operator against an instance of the GroupPermission CR
+type Condition struct {
+	// LastTransitionTime is the last time this condition was active for the CR
+	LastTransitionTime metav1.Time `json:"lastTransitionTime"`
+	// Message related to the condition
+	// +optional
+	Message string `json:"message,omitempty"`
+	// ClusterRoleName in which this condition is true
+	ClusterRoleName string `json:"clusterRoleName"`
+	// Flag to indicate if condition status is currently active
+	Status bool `json:"status"`
+	// State that this condition represents
+	State string `json:"state"`
+}
+
+// GroupPermissionState defines various states a GroupPermission CR can be in
+type GroupPermissionState string
+
+const (
+	// GroupPermissionCreated const for Created status
+	GroupPermissionCreated GroupPermissionState = "Created"
+	// GroupPermissionCreated const for Failed status
+	GroupPermissionFailed GroupPermissionState = "Failed"
+)
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
