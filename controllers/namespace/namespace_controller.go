fa618a4966b40390577146cead325e87fc76b084
Ravi Trivedi <travi@redhat.com>
2023-01-23 13:27:34 +0530

    Revert "Enabling metrics for cluster and namespace permissions"
    This reverts commit 03f4e2819bae88c5e8b46fd76c86baab0b6ebefc.

--- controllers/namespace/namespace_controller.go
+++ controllers/namespace/namespace_controller.go
@@ -30,7 +30,6 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	managedv1alpha1 "github.com/openshift/rbac-permissions-operator/api/v1alpha1"
-	"github.com/openshift/rbac-permissions-operator/pkg/metrics"
 )
 
 var log = logf.Log.WithName("controller_namespace")
@@ -122,7 +121,6 @@ func (r *NamespaceReconciler) Reconcile(ctx context.Context, request ctrl.Reques
 					reqLogger.Error(err, failedToCreateRoleBindingMsg)
 					return ctrl.Result{}, err
 				}
-				metrics.AddPrometheusMetric(&subPerm)
 				roleBindingName := fmt.Sprintf("%s-%s", permission.ClusterRoleName, subjectPermission.Spec.SubjectName)
 				reqLogger.Info(fmt.Sprintf("RoleBinding %s created successfully in namespace %s", roleBindingName, instance.Name))
 			}
